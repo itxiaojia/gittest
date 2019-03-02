0a0a92a46956f548a08d94da0d646c885eeb0736
itxiaojia <itxiaojia@163.com>
2019-03-02 17:13:56 +0800

    git练习 git -add  git -commit

---
+++ src/main/java/com/baidu/Test01.java
@@ -0,0 +1,14 @@
+package com.baidu;
+
+
+/**
+ * @Author: Mr.jia
+ * @Date: 2019/3/2 17:12
+ */
+public class Test01 {
+
+	public static void main(String[] args) {
+
+		System.out.println("Hello git");
+	}
+}
